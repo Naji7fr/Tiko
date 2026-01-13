8bdab6a2384e6a9da23acce886fb090fdb137fd1
Naji <338718@student.mboutrecht.nl>
2026-01-13 09:33:44 +0100

    Add migration for creating medewerkers table with necessary columns

---
+++ database/migrations/2025_11_21_095939_create_medewerkers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medewerkers', function (Blueprint $table) {
+            $table->id();
+            $table->string('naam');
+            $table->string('email')->unique();
+            $table->enum('status', ['Actief', 'Inactief', 'Op proef', 'Afwezig', 'Gepauzeerd']);
+            $table->enum('type', ['Tandarts', 'Mondhygienist', 'Assistent', 'Praktijkmanagement']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medewerkers');
+    }
+};
